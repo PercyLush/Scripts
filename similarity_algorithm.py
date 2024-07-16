c7f11ca94693a46277cfcd336b6cde478f074e18
Bheki Lushaba <bheki@gradesmatch.com>
2024-07-16 15:42:34 +0200

    test

---
+++ similarity_algorithm.py
@@ -0,0 +1,5 @@
+# Create a search bar algorithm, that queries the Database
+# Displays/prints from most similar to least similar Using first_name and last_name
+
+name = ["Percy Lushaba", "Percival Sibiya", "Bheki Lushaba", "Peaches Khoza", "Percy Stander"]
+#To be continued...
--- testing.py
+++ testing.py
@@ -1,7 +1,11 @@
-from pymongo import MongoClient
+# my_list = [1, 2, 3]
+# for v in range(len(my_list)):
+#     my_list.insert(1, my_list[v])
+# print(my_list)
 
-Client = MongoClient("")
-db = Client["Folder_Name"]
-collection = db["Collection_List"]
 
+# new_list = [1, 2, 3, 4]
+# print(new_list[-3:-2])
 
+my_list = [[hour for hour in range(1, 25)] for day in range(1, 32)]
+print(my_list)
