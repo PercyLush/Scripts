aa70eec34c4f3da6bba2351e96cfac17dced87b4
Bheki Lushaba <bheki@gradesmatch.com>
2024-08-21 13:46:41 +0200

    add

--- extract.py
+++ extract.py
@@ -1,16 +1,16 @@
 import PyPDF2
 
-path = "C:\\Users\\Bheki Lushaba\\Downloads\\Rule-book-2024-FINAL (2).pdf"
+path = "C:\\Users\\Bheki Lushaba\\Downloads\\APPM323_Module Plan 2024 - Kamogelo Makuapane.pdf"
 
 with open(path, "rb") as file1:
     data = PyPDF2.PdfReader(file1)
 
     Text = ""
 
-    for page in range(126, 174):
+    for page in range(1, 1):
         pages = data.pages[page]
         text = pages.extract_text()
         Text += text
 
-with open("C:\\Users\\Bheki Lushaba\\Desktop\\Scripts\\data.txt", "w", encoding="utf-8") as file2:
+with open("C:\\Users\\Bheki Lushaba\\Desktop\\Scripts\\APPM323.txt", "w", encoding="utf-8") as file2:
     file2.write(Text)
---
+++ no_name_courses.py
@@ -0,0 +1,14 @@
+import json
+
+path = "C:\\Users\\Bheki Lushaba\\course-data\\CourseData_Final\\University of Limpopo.json"
+
+with open(path, "r", encoding="utf-8") as file1:
+    data = json.load(file1)
+    Text = ''
+    for item in data:
+        if "Name" not in item:
+            if "Code" in item:
+                Text += f"{item['Code']}\n"
+
+with open("C:\\Users\\Bheki Lushaba\\Desktop\\Scripts\\no_name_courses.txt", "w", encoding="utf-8") as file2:
+    file2.write(Text)
