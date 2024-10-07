c36616a5bd63d795b67eef69c8eb45ed1de0ea91
Bheki Lushaba <bheki@gradesmatch.com>
2024-10-07 15:26:51 +0200

    pattern

--- pattern_hackerrank.py
+++ pattern_hackerrank.py
@@ -1,18 +1,3 @@
-# Sample Input
-
-# 9 27
-# Sample Output
-
-# ------------.|.------------
-# ---------.|..|..|.---------
-# ------.|..|..|..|..|.------
-# ---.|..|..|..|..|..|..|.---
-# ----------WELCOME----------
-# ---.|..|..|..|..|..|..|.---
-# ------.|..|..|..|..|.------
-# ---------.|..|..|.---------
-# ------------.|.------------
-    
 # Size: 11 x 33
 # ---------------.|.---------------
 # ------------.|..|..|.------------
@@ -26,35 +11,35 @@
 # ------------.|..|..|.------------
 # ---------------.|.---------------
 
-# Size: 7 x 21 
-# ---------.|.---------
-# ------.|..|..|.------
-# ---.|..|..|..|..|.---
-# -------WELCOME-------
-# ---.|..|..|..|..|.---
-# ------.|..|..|.------
-# ---------.|.---------
+try:
+    N, M = int(input()).split()
+    print(f"{N}, {M}")
+    welcome_number = int((N - 1)/2 + 1)
 
-N = int(input())
-M = 3 * N
+    counter1 = 1
+    dash = 3
+    dash1 = 1
+    test1 = M
+    test2 = M
 
-welcome_index = ((N - 1)/2) + 1
-welcome_dash = int((M - 7)/2)
-stick_num1 = 0
-counter = 0
-for num in range(1, N + 1):
+    if 5 < N < 101 and 15 < M < 303:
+        for length in range(1, N + 1):
+            
+            if length == welcome_number:
+                num = int((M - 7)/2)
+                print(f"{'-'*num}{'WELCOME'}{'-'*num}")
 
-    if num < welcome_index:
-        #print("-", end="")
-        print(".|."*(num+stick_num1))
-        #print("-")
-        stick_num1 += 1
-
-    elif num == welcome_index:
-        print(f'{"-"*welcome_dash}{"WELCOME"}{"-"*welcome_dash}')
-    
-    elif num > welcome_index:
-        #print("-", end="")
-        print(".|."*(num-counter))
-        #print("-")
-        counter += 3
+            else:
+                if length < welcome_number:
+                    dash_num = int((test1 - dash)/2)
+                    print(f"{'-'*dash_num}{'.|.'*counter1}{'-'*dash_num}")
+                    counter1 += 2
+                    dash += 3
+                    test1 -= 3
+                elif length > welcome_number:
+                    counter1 -= 2
+                    dash_num = test2 - int(test2 - 3*dash1)
+                    print(f"{'-'*dash_num}{'.|.'*counter1}{'-'*dash_num}")
+                    dash1 += 1
+except:
+    print("Error...")
